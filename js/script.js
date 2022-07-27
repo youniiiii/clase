f7c80e22f577e9412ed94af8090dd477acbb8e6a
jonatan mansilla <yoniii-11@live.com.ar>
2022-07-27 18:22:44 -0300

    subida

--- js/script.js
+++ js/script.js
@@ -1,32 +1,31 @@
 //alert('compra de una entrada de boliche <br> solo tiene  tres intentos');
 
 
-let nombre1 = document.getElementById('#nombre').value;
-let apellido1 = document.getElementById('#apellido').value;
-let edad1 = document.getElementById('#edad').value;
-let dinero1 = document.getElementById('#pagar').value;
-let entradas1 = document.getElementById('#entradas').value;
+////let apellido1 = document.getElementById('#apellido').value;
+//let edad1 = document.getElementById('#edad').value;
+//let dinero1 = document.getElementById('#pagar').value;
+//let entradas1 = document.getElementById('#entradas').value;
 
-const precio = 300;
+//const precio = 300;
 
-function resta(n, nn) {
-   resultado = n - nn;
-   return resultado;
-}
+//function resta(n, nn) {
+  // resultado = n - nn;
+  // return resultado;
+//}
 
 
-const teket = {
-   nombre: 'nombre',
-   apellido: 'apellido',
-   edad: 'edad',
-   vuelto: 'vuelto'
-}
+//const teket = {
+//   nombre: 'nombre',
+//   apellido: 'apellido',
+//   edad: 'edad',
+//   vuelto: 'vuelto'
+//}
 
-for (let i = 0; i < 3; i++) {
+//for (let i = 0; i < 3; i++) {
 
-   if (edad1 > 18 && entradas1 <= 3 && dinero1 > precio) {
-   }
-}
+  // if (edad1 > 18 && entradas1 <= 3 && dinero1 > precio) {
+  // }
+//}
 
 
 function saltearlinia() { document.write("<br>") }
@@ -36,23 +35,25 @@ function mostrar(frase) {
 function solicitar(dato) {
    var solicitud = prompt(dato)
 }
-
+alert('nombre');
 contraseha = solicitar("porfavor dicte su contraseña");
 usuario = solicitar("porfavor dictesu usuario")
-var contra = "master"
-var usuar = "master"
+let contra = "master"
+let usuar = "master"
 
-if (contra == contraseha && usuar == usuario) {
+let limite = 3;
+
+for (let i = 0;i < limite ; i++){
+   if (contra == contraseha && usuar == usuario) {
    mostrar("  bienvenido")
    mostrar("  usaste esta contraseña  " + contra + " y este  usuari  " + usuar)
-}
-
-
-else {
-
+}else {
    mostrar(" incorreecto usaste una contraseña  erronea desiste usar esto " + usuario + "  y  la contraseña " + contraseha);
    mostrar("intentalo mas tarde")
 }
+
+}
+
 
 
 
