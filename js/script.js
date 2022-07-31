185c96693b8143f8068b2febcd50258999603fc5
jonatan mansilla <yoniii-11@live.com.ar>
2022-07-31 20:39:02 -0300

    subida de  actualizacion

--- js/script.js
+++ js/script.js
@@ -34,49 +34,34 @@ function mostrar(frase) {
 }
 
 let user = ['master', 'yoni', 'admin'];
-let pass = ['master', 'yoni','admin' ];
+let pass = ['master', 'yoni', 'admin'];
 
-function regreso() {
-}
+
 let limite = 3;
 
 
 
 
 for (let i = 0; i < pass.length; i++) {
-
    let contraseha = prompt("porfavor dicte su contraseña");
    let usuario = prompt("porfavor dicte su usuario");
    let resta = limite - i;
    alert('LE QUEDA ' + resta + ' intentos ');
-   mostrar(limite - i);
    for (let io = 0; io < pass.length; io++) {
       const pos = pass[io];
-      const contra = user[io] ;
-   if (pos == contraseha && contra == usuario) {
-      mostrar("  bienvenido " + contra);
-      
-   } else {
-      mostrar(" incorrecto usaste una contraseña  erronea debiste usar esto " + contra + "  y  la contraseña " + pos);
-      mostrar("intentalo mas tarde")
+      const contra = user[io];
+       if (pos == contraseha && contra == usuario) {
+         mostrar("  bienvenido " + contra);
+         i=10;
+         break;
 
-   }
-   break;
-   }
-   break;
-   
+      } else {
+         mostrar('te quedan : '+resta+ ' intentos. ');
+         mostrar(" incorrecto usaste una contraseña  erronea debiste usar esto " + contra + "  y  la contraseña " + pos);
+         mostrar("intentalo mas tarde")
 
+      }break;
+   }
 }
-
-
-
-
-
-
-
-
-
-
-
 
 
